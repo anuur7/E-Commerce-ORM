36699b2c32b6bd921c6bedd14d0c9fe909d4e33a
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-03-25 15:40:14 +0000

    created the two get routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,15 +2,25 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({include:[Product]})
+  .then((categories) => {res.json(categories)})
+  .catch((err) => {res.status(500).json(err)})
+
+
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne(
+    {
+      where: {id: req.params.id},
+      include:[Product]})
+  .then((categories) => {res.json(categories)})
+  .catch((err) => {res.status(500).json(err)})
 });
 
 router.post('/', (req, res) => {
